6476de051d212214d771f1ce61c4b1929ed09c8a
JeanNesi <jean.carlos_nesi@hotmail.com>
2023-10-31 19:17:27 -0300

    feat: services

--- src/main/java/com/apera/aperaserver/service/CompraService.java
+++ src/main/java/com/apera/aperaserver/service/CompraService.java
@@ -19,20 +19,7 @@ um registro detalhado do lançamento, para exclusão ou edição posteriormente.
 
     @Transactional
     public String salvarCompra(Compra compra) {
-        try {
-            validarCompra(compra); // Adicione a validação
-
-            Compra compraSalva = compraRepository.save(compra);
-			
-			if (compra == null || compra.getValor() <= 0) {
-                    throw new IllegalArgumentException("A compra é inválida");
-            }
-		
-            logInfo("Compra salva com sucesso: ID " + compraSalva.getId());
-
-            return "Compra bem sucedida");
-        } catch (Exception e) {
-            return "Erro ao salvar a compra";
-        }
+        compraRepository.save(compra);
+        return "Compra bem sucedida";
     }
 }
